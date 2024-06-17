97650f7db112f87d6978dae8c9a412edcf5b17b4
botirovdev <botirovdev7@gmail.com>
2024-06-17 14:55:23 +0500

    fixed bugs

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -5,9 +5,12 @@ const Rates = () => {
     const today = new Date();
     const formattedDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
     console.log(formattedDate);
+
     const [dollarRate, setDollarRate] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         const fetchExchangeRates = async () => {
             try {
                 const response = await axios.get('https://cbu.uz/uz/arkhiv-kursov-valyut/json/');
@@ -18,24 +21,36 @@ const Rates = () => {
         };
 
         fetchExchangeRates();
+        setLoading(false)
     }, []);
     return (
-        <section className="rates">
-            <div className="container">
-                <h1 className="rates-title">{formattedDate} sanasiga oid valyuta kurslari</h1>
-                <div className="rates-content">
-                    {dollarRate.map(rate => (
-                        <div className='rates-rate' key={rate.Ccy}>
-                            <div className='rates-items'>
-                                <h1 className='rates-region'>{rate.Ccy}</h1>
-                                <h1 className='rates-name'>1 {rate.CcyNm_UZ}</h1>
+        <div>
+            {
+                loading ? (
+                        <h1>Yuklanmoqda</h1>
+                ) : (
+                    <section className="rates">
+                        <div className="container">
+                            <h1 className="rates-title">{formattedDate} sanasiga oid valyuta kurslari</h1>
+
+                            <div className="rates-content">
+                                {dollarRate.map(rate => (
+                                    <div className='rates-rate' key={rate.Ccy}>
+                                        <div className='rates-items'>
+                                            <h1 className='rates-region'>{rate.Ccy}</h1>
+                                            <h1 className='rates-name'>1 {rate.CcyNm_UZ}</h1>
+                                        </div>
+                                        <h1 className='rates-price'>{parseFloat(rate.Rate).toFixed(2)} so'm</h1>
+                                    </div>
+                                ))}
                             </div>
-                            <h1 className='rates-price'>{parseFloat(rate.Rate).toFixed(2)} so'm</h1>
                         </div>
-                    ))}
-                </div>
-            </div>
-        </section>
+
+                    </section>
+                )
+            }
+
+        </div>
     )
 }
 
